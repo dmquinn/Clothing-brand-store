a908fb0659f8cee6318eda2ad444584d7623d8b3
dmquinn <danielmurphyberling@gmail.com>
2021-03-19 22:23:16 +0100

    product rows separated by category

--- frontend/src/screens/AllProductsScreen.js
+++ frontend/src/screens/AllProductsScreen.js
@@ -9,16 +9,27 @@ import Loader from "../components/Loader";
 import { listProducts } from "../actions/productActions";
 
 const HomeScreen = ({ match }) => {
-	const [margin, setMargin] = useState(100);
-	const [shadow, setShadow] = useState(8);
+	const [marginJackets, setMarginJackets] = useState(210);
+	const [shadowJackets, setShadowJackets] = useState(8);
+	const [marginTrousers, setMarginTrousers] = useState(100);
+	const [shadowTrousers, setShadowTrousers] = useState(8);
 
 	const keyword = match.params.keyword;
 	const pageNumber = match.params.pageNumber || 1;
-
+	let jackets = [];
+	let trousers = [];
 	const dispatch = useDispatch();
 	const productList = useSelector((state) => state.productList);
 
 	const { loading, error, products } = productList;
+	if (products) {
+		jackets = products.filter(function (product) {
+			return product.category === "Jackets";
+		});
+		trousers = products.filter(function (product) {
+			return product.category === "Trousers";
+		});
+	}
 
 	useEffect(() => {
 		dispatch(listProducts(keyword, pageNumber));
@@ -31,8 +42,6 @@ const HomeScreen = ({ match }) => {
 					Back
 				</Link>
 
-				{!keyword && <h1 className="mt-5">Jackets</h1>}
-
 				<h1 className="mt-3">{keyword}</h1>
 				{loading ? (
 					<Loader />
@@ -40,38 +49,86 @@ const HomeScreen = ({ match }) => {
 					<Message variant="danger">{error}</Message>
 				) : (
 					<>
+						{!keyword && (
+							<h1 className="mt-5">Latest Collections</h1>
+						)}
 						<button
 							className="offset-1 arrow"
 							onMouseDown={() => {
-								setMargin(margin + 200);
-								setShadow(shadow + 10);
+								setMarginJackets(marginJackets + 200);
+								setShadowJackets(shadowJackets + 10);
 							}}
 						>
-							<i class="fas fa-arrow-left"></i>
+							<i className="fas fa-arrow-left"></i>
 						</button>
 						<button
 							className="offset-1 arrow"
-							// style={{ backgroundColor: "none" }}
 							onMouseDown={() => {
-								setMargin(margin - 200);
-								setShadow(shadow - 10);
+								setMarginJackets(marginJackets - 200);
+								setShadowJackets(shadowJackets - 10);
 							}}
 						>
-							<i class="fas fa-arrow-right"></i>
+							<i className="fas fa-arrow-right"></i>
 						</button>
 						<div
 							className="sliderContainer"
 							style={{
-								marginLeft: `${margin}px`,
+								marginLeft: `${marginJackets}px`,
 								transition: "0.7s ease-out",
-								filter: `drop-shadow(${shadow}px 2px 25px rgba(0, 0, 0, 0.233))`,
+								filter: `drop-shadow(${shadowJackets}px 22px 5px rgba(0, 0, 0, 0.233))`,
 							}}
 						>
 							<div className="dontClip">
 								<Row>
-									{products.map((product, i) => (
-										<Product key={i} product={product} />
-									))}
+									{!!jackets &&
+										jackets.map((product, i) => (
+											<Product
+												key={i}
+												product={product}
+												name={"Sport Goth"}
+											/>
+										))}
+								</Row>
+							</div>
+						</div>
+
+						{!keyword && <h1 className="mt-5">Trousers</h1>}
+						<button
+							className="offset-1 arrow"
+							onMouseDown={() => {
+								setMarginTrousers(marginTrousers + 200);
+								setShadowTrousers(shadowTrousers + 10);
+							}}
+						>
+							<i className="fas fa-arrow-left"></i>
+						</button>
+						<button
+							className="offset-1 arrow"
+							onMouseDown={() => {
+								setMarginTrousers(marginTrousers - 200);
+								setShadowTrousers(shadowTrousers - 10);
+							}}
+						>
+							<i className="fas fa-arrow-right"></i>
+						</button>
+						<div
+							className="sliderContainer"
+							style={{
+								marginLeft: `${marginTrousers}px`,
+								transition: "0.7s ease-out",
+								filter: `drop-shadow(${shadowTrousers}px 22px 5px rgba(0, 0, 0, 0.233))`,
+							}}
+						>
+							<div className="dontClip">
+								<Row>
+									{!!trousers &&
+										trousers.map((product, i) => (
+											<Product
+												key={i}
+												product={product}
+												name={"Sport Goth"}
+											/>
+										))}
 								</Row>
 							</div>
 						</div>
